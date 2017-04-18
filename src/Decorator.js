c76a1028fad8cae98a654a7c76ee349e6ab7829c
Alex Kit <source.kit@gmail.com>
2017-04-19 00:15:43 +0200

    fix (BemModuleResolver) check node ast first

--- src/Decorator.js
+++ src/Decorator.js
@@ -5,40 +5,112 @@ mask.defineDecorator('bem', {
 		this.name = name;
 	},
 	beforeRender (node, model, ctx, el, ctr) {
-		var bemModule = getBemModule(node, ctr, this.name);
-		if (bemModule != null) {
-			bemModule.bemCss.transformAst(node);
+		let result = getBemModule(node, ctr, this.name);
+		if (result == null) {
+			return;
+		}
+		let [ module, importNode ] = result;
+		
+		module.bemCss.transformAst(node);
+		if (mask.is.NODE) {
+			// for later serialization
+			importNode.attr = { salt: module.bemCss.getSalt() };
 		}
 	}
 });
 
-var arr = [];
-function getBemModule (node, ctr, name) {
-	var module = null;
-	for(var cursor = ctr; cursor != null; cursor = cursor.parent) {
-		if (cursor.compoName !== 'imports') {
-			continue;
+let getBemModule;
+(function () {
+	const getBemModule_Return = [];
+	getBemModule = function (node, ctr, name) {
+		if (tryGetFromAst(node, name)) {
+			return getBemModule_Return;
+		}
+		if (tryGetFromCompos(ctr, name)) {
+			return getBemModule_Return;
 		}
 
-		var imports = cursor.imports_;
-		for(var i = 0; i < imports.length; i++) {
-			var import_ = imports[i];
-			if (import_.moduleType !== 'bem') {
-				continue;
-			}
-			if (name != null && import_.alias !== name) {
-				continue;
-			}
-			if (module != null) {
-				mask.log.errorWithNode('Ambiguous BEM import found', import_);
-				return null;
-			}
-			module = import_.module;
-		}
-		break;
-	}
-	if (module == null) {
 		mask.log.errorWithNode('BEM import not found', node);
+		return null;
+	};
+
+	function tryGetFromCompos (ctr, name) {
+		let module = null, importNode = null;
+		for(let cursor = ctr; cursor != null; cursor = cursor.parent) {
+			if (cursor.compoName !== 'imports') {
+				continue;
+			}
+
+			var imports = cursor.imports_;
+			for(var i = 0; i < imports.length; i++) {
+				var import_ = imports[i];
+				if (import_.moduleType !== 'bem') {
+					continue;
+				}
+				if (name != null && import_.alias !== name) {
+					continue;
+				}
+				if (module != null) {
+					mask.log.errorWithNode('Ambiguous BEM import found', import_);
+					return null;
+				}
+				module = import_.module;
+				importNode = cursor.nodes[i];
+			}
+			break;
+		}
+		if (module != null) {
+			getBemModule_Return[0] = module;
+			getBemModule_Return[1] = importNode;
+			return true;
+		}
+		return false;
 	}
-	return module;
-}
+	function tryGetFromAst (node, name) {
+		let module = null, importNode = null;
+		for(let cursor = node; cursor != null; cursor = cursor.parent) {
+			if (cursor.tagName !== 'imports') {
+				continue;
+			}
+
+			let nodes = cursor.nodes;
+			for(let i = 0; i < nodes.length; i++) {
+				let x = nodes[i];
+				if (x.tagName !== 'import') {
+					continue;
+				}
+				let import_ = x;
+				if (import_.moduleType !== 'bem') {
+					continue;
+				}
+				if (name != null && import_.alias !== name) {
+					continue;
+				}
+				if (module != null) {
+					mask.log.errorWithNode('Ambiguous BEM import found', import_);
+					return null;
+				}				
+				importNode = import_;
+				break;
+			}
+		}
+		if (importNode != null) {
+			let root;
+			for(let cursor = importNode; cursor != null; cursor = cursor.parent) {
+				if (cursor.parent == null) {
+					root = cursor;
+				}
+			}
+			if (root && root.filename) {
+				module = mask.Module.createModule(importNode, root);
+			}
+		}
+		if (module != null) {
+			getBemModule_Return[0] = module;
+			getBemModule_Return[1] = importNode;
+			return true;
+		}
+		return false;
+	}
+}());
+
--- src/Module.js
+++ src/Module.js
@@ -1,10 +1,12 @@
 let AutoBem = require('auto-bem');
 
 mask.Module.registerModuleType('style', 'bem', {
+	bemCss: null,
 	load_ () {
 		return mask.File.get(this.path).then(style => {
 			this.bemCss = new AutoBem.BemCss(style, {
-				filename: this.path
+				filename: this.path,
+				salt: this.attr && this.attr.salt
 			});
 
 			if (mask.is.NODE) {
